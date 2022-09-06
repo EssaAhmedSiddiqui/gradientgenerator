f4e89a8414263aa135be28465e389c49282a9718
Essa Ahmed Siddiqui <100959596+EssaAhmd@users.noreply.github.com>
2022-09-06 10:33:36 +0500

    Update script.js
    Changed function to arrow function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
-function setGradient() {
+let setGradient = () => {
 	body.style.background = 
 	"linear-gradient(to right, " 
 	+ color1.value 
